73c5c4c7336d878205da735a36998a0eabd17e30
Felix C. Morency <1102868+fmorency@users.noreply.github.com>
2025-06-09 15:12:03 -0400

    chore: refactor for dry

--- src/lib/loaders/loadCumsumMetric.ts
+++ src/lib/loaders/loadCumsumMetric.ts
@@ -2,6 +2,6 @@ import {createDataLoader} from "./createDataLoader";
 
 export function loadCumsumMetric(ids: string[]) {
   return createDataLoader(ids, (id, params) =>
-    `/rpc/get_cumsum_${id}?${params.toString()}`
+    `/rpc/get_cumsum_agg_${id}?${params.toString()}`
   );
 }
