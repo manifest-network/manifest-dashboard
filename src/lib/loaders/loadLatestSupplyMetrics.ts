d274236a5e4a40345e2c8957d10be87bd9489644
Felix C. Morency <1102868+fmorency@users.noreply.github.com>
2025-06-16 10:25:12 -0400

    fix: good bot

--- src/lib/loaders/loadLatestSupplyMetrics.ts
+++ src/lib/loaders/loadLatestSupplyMetrics.ts
@@ -4,6 +4,6 @@ import {makeSingleMetricValueSchema} from "$lib/schemas/metricRecord";
 export function loadLatestSupplyMetric(network: NetworkType, id: string) {
   return createSingleLoader(
     () => `/rpc/get_latest_${network}_${id}`,
-    makeSingleMetricValueSchema(`${id}`),
+    makeSingleMetricValueSchema(id),
   );
 }
