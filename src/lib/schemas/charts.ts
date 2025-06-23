1b6126e8767d0500c61dcb9f72d61d3a4062c899
Felix C. Morency <1102868+fmorency@users.noreply.github.com>
2025-06-23 09:45:54 -0400

    fix: memoize schemas to avoid memleaks

--- src/lib/schemas/charts.ts
+++ src/lib/schemas/charts.ts
@@ -1,6 +1,7 @@
 import {z} from "zod/v4";
-import {makePreprocessedMetricRecordSchema} from "$lib/schemas/metricRecord";
+import {makePreprocessedMetricRecordSchema, type MetricRecord} from "$lib/schemas/metricRecord";
 import {bigNumberLike} from "$lib/schemas/common";
+import memoize from "lodash/memoize";
 
 // A single data point for internal charts, representing a metric at a specific time
 export const ChartDataPointSchema = z.object({
@@ -10,21 +11,25 @@ export const ChartDataPointSchema = z.object({
   date: z.date(),
 })
 
-export type ChartDataPoint = z.infer<typeof ChartDataPointSchema>;
+export type ChartDataPoint = z.infer<typeof ChartDataPointSchema>
+
+function preprocessPoints(group: string, arr: MetricRecord[]): ChartDataPoint[] {
+  return arr.map((r) => {
+    const date = new Date(r.timestamp);
+    return ChartDataPointSchema.parse({
+      group,
+      key: date.toISOString(),
+      value: r.value,
+      date,
+    });
+  });
+}
 
 // Factory for a schema that transforms a `MetricRecord[]` (from the API) to a `ChartDataPoint[]` (for internal charts).
-export const ChartDataPointArraySchema = (group: string) => {
-  const PreprocessedMetricArray = z.array(makePreprocessedMetricRecordSchema(group));
-  return PreprocessedMetricArray.transform((arr) =>
-    arr.map((r) => {
-      const date = new Date(r.timestamp);
-
-      return ChartDataPointSchema.parse({
-        group,
-        key: date.toISOString(),
-        value: r.value,
-        date,
-      });
-    })
-  )
-}
+export const ChartDataPointArraySchema = memoize(
+  (group: string) => {
+    return z
+      .array(makePreprocessedMetricRecordSchema(group))
+      .transform((arr) => preprocessPoints(group, arr))
+  }
+)
