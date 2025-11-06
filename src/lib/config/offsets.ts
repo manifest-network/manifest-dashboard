31ca5b4e99ab94072ee1e5b5ad867cf88f0c95ad
Felix C. Morency <1102868+fmorency@users.noreply.github.com>
2025-11-06 08:31:27 -0500

    fix: offsets

--- src/lib/config/offsets.ts
+++ src/lib/config/offsets.ts
@@ -2,7 +2,8 @@
 // Any work performed on these metrics BEFORE the mainnet launch date should NOT be counted.
 import {bigNumberLike} from "$lib/schemas/common";
 
+// Offsets from September 21 2025 00:00:00 UTC
 export const METRIC_OFFSETS: Partial<Record<string, string>> = {
-  total_mfx_burned: bigNumberLike.parse("135304300855749600000"),
-  locked_fees: bigNumberLike.parse("162936737"),
+  total_mfx_burned: bigNumberLike.parse("135304300855652060000"),
+  locked_fees: bigNumberLike.parse("156969458"),
 }
